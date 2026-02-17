bbdeb6768f4815bddaadd94a0293b6c5261cd196
Niharika <niharika.juturu@gmail.com>
2026-02-17 23:03:16 +0530

    Alignment

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -59,7 +59,7 @@ const Browse = () => {
             <Navbar />
             <Billboard era={billboardFilm} />
 
-            <div className="pb-40 relative z-20 pl-4 md:pl-16 -mt-10 md:-mt-24 lg:-mt-32 space-y-8 md:space-y-16">
+            <div className="pb-40 relative z-20 pl-4 md:pl-16 -mt-4 md:-mt-10 lg:-mt-16 space-y-8 md:space-y-16">
 
                 {/* Primary Row: Specific to this Profile */}
                 {profileFilms.length > 0 && (
